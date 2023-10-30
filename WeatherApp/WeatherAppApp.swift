a520000430085d70216f9b8fcd8132081dd8e15b
Ilya Ovchinnikov <jewovch@gmail.com>
2023-10-30 10:10:11 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ilya Ovchinnikov on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
